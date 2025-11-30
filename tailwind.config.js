69fdc01449397c55ebc73849ecb312eeb4fb9722
Josué Calderón <calderonjosue_@MacBook-Pro-de-Josue.local>
2025-11-30 12:11:36 -0500

    Inicialización final del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <--- ¡Esto es lo importante!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
